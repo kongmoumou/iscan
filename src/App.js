dbddcec8fbc26214bae78c7bcf723e0fc3cfba6f
kongmoumou <kongmoumou666@gmail.com>
2020-10-28 02:47:11 +0800

    feat: update `App`

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React, { useEffect, useLayoutEffect, useRef, useCallback, useState } from 'react';
+import React, {
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useCallback,
+  useState,
+} from 'react';
 import './App.scss';
 import Webcam from 'react-webcam';
 import Cropper from 'react-cropper';
 import 'cropperjs/dist/cropper.css';
-import useSWR from 'swr';
-import axios from './utils/axios';
 import { useSpring, animated as a } from 'react-spring';
+import { useUpdateEffect } from 'react-use';
+
+// TODO: update
+import Result from './components/Result';
+
+// Swiper
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper.scss';
 
 // face detection
 import * as faceApi from 'face-api.js';
 import { interpolateAgePredictions } from './utils/faceHelper';
+import AnimeApi from './components/AnimeApi';
 
 const videoConstraints = {
   // NOTE: width -> height ?
@@ -22,12 +35,6 @@ const frontVideoConstraints = {
   height: window.innerWidth * devicePixelRatio * 2,
   width: window.innerHeight * 0.7 * devicePixelRatio * 2,
   facingMode: 'user',
-};
-
-const fetchAnimeSearchRes = (image) => {
-  return axios.post('https://trace.moe/api/search', {
-    image,
-  });
 };
 
 const AniemePreview = ({ data }) => {
@@ -50,7 +57,7 @@ const AniemePreview = ({ data }) => {
   );
 };
 
-const WebcamCapture = () => {
+const WebcamCapture = ({ onCropDone }) => {
   const webcamRef = useRef(null);
   const cropperRef = useRef();
 
@@ -82,7 +89,10 @@ const WebcamCapture = () => {
     setCroppedImg(null);
   }, []);
 
-  const { data, isValidating, error } = useSWR(croppedImg, fetchAnimeSearchRes);
+  useUpdateEffect(() => {
+    // eslint-disable-next-line no-unused-expressions
+    typeof onCropDone === 'function' ? onCropDone(croppedImg) : null;
+  }, [croppedImg]);
 
   useEffect(() => {
     if (webcamRef.current) {
@@ -127,7 +137,6 @@ const WebcamCapture = () => {
     }
 
     async function detectFace() {
-
       const videoEl = webcamRef.current.video;
 
       const result = await faceApi
@@ -142,14 +151,14 @@ const WebcamCapture = () => {
         const canvas = document.querySelector('.face__overlay');
         if (canvas) {
           const dims = faceApi.matchDimensions(canvas, videoEl, true);
-  
+
           const resizedResult = faceApi.resizeResults(result, dims);
 
-          console.log(resizedResult)
+          console.log(resizedResult);
           // draw box
           faceApi.draw.drawDetections(canvas, resizedResult);
           const { age, gender, genderProbability } = resizedResult;
-  
+
           // interpolate gender predictions over last 30 frames
           // to make the displayed age more stable
           const interpolatedAge = interpolateAgePredictions(age);
@@ -184,15 +193,6 @@ const WebcamCapture = () => {
         <img src={croppedImg} style={{ width: '100%' }} />
         <div>
           <button onClick={handleBackClick}>back</button>
-        </div>
-        <div>
-          {isValidating && 'loading...'}
-          {!isValidating &&
-            !!data?.data?.docs?.length &&
-            data.data.docs.map((doc, i) => (
-              <AniemePreview key={i} data={doc} />
-            ))}
-          {error ? String(error) : null}
         </div>
       </>
     );
@@ -274,9 +274,42 @@ const WebcamCapture = () => {
 };
 
 function App() {
+  const [shouldShowDrawer, setShouldShowDrawer] = useState(0);
+  const [img, setImg] = useState();
+
   return (
     <div className="App">
-      <WebcamCapture />
+      <WebcamCapture
+        onCropDone={(croppedImg) => {
+          setImg(croppedImg);
+        }}
+      />
+      <button
+        onClick={() => {
+          setShouldShowDrawer((state) => state + 1);
+        }}
+      >
+        show drawer
+      </button>
+      {!!shouldShowDrawer && <Result key={shouldShowDrawer} />}
+      <div className="selector__container">
+        <Swiper
+          className="selector__swiper"
+          centeredSlides
+          slideToClickedSlide
+          spaceBetween={50}
+          slidesPerView="auto"
+          onSlideChange={(e) => console.log('slide change', e)}
+          // onSwiper={(swiper) => console.log(swiper)}
+        >
+          <SwiperSlide>识物</SwiperSlide>
+          <SwiperSlide>识文</SwiperSlide>
+          <SwiperSlide>识人</SwiperSlide>
+          <SwiperSlide>识番</SwiperSlide>
+        </Swiper>
+        <div className="selector__indicator" />
+      </div>
+      {img && <AnimeApi img={img} />}
     </div>
   );
 }
